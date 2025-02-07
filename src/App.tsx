b41ff0843fa2204d872ec8c11265d6df81f503e5
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-02-07 14:33:23 +0100

    Add promo banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <h1>Starting Point</h1>;
+  return (
+    <>
+      <div
+        className="py-5 text-center font-medium bg-[#292929] text-[#efefef]"
+        style={{
+          letterSpacing: ".4px",
+        }}
+      >
+        <p>Promo: Free shipping in every orders</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
