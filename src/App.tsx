96c21d9c8e658771293fd5c06a7b05f5bc9a3fe7
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-02-17 17:55:50 +0100

    Add women's collection section

--- src/App.tsx
+++ src/App.tsx
@@ -436,6 +436,132 @@ function App() {
           ))}
         </div>
       </section>
+
+      <section className="w-[1200px] max-w-full mx-auto mt-14">
+        <span
+          className="text-[#a74107] text-4xl block tracking-[-4px] text-center"
+          style={{ fontFamily: "Helvetica" }}
+        >
+          women
+        </span>
+
+        <h2
+          className="text-center uppercase text-5xl sm:text-7xl lg:text-8xl xl:text-[166px] tracking-[-4px] font-normal"
+          style={{
+            fontFamily: "helvetica",
+          }}
+        >
+          Collection
+        </h2>
+
+        <div className="product__grid">
+          <div
+            className="p-4 border border-solid border-[#bdbdbd] hidden xl:block"
+            style={{ gridArea: "bg-1" }}
+          >
+            <img className="collection_image" src="/women.jpg" alt="" />
+          </div>
+
+          {products.map((product) => (
+            <div key={product.id} className="product text-center xl:text-left">
+              <div className="love-icon right-5">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="transparent"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12.62 20.9641C12.28 21.0841 11.72 21.0841 11.38 20.9641C8.48 19.9741 2 15.8442 2 8.84415C2 5.75415 4.49 3.25415 7.56 3.25415C9.38 3.25415 10.99 4.13415 12 5.49415C13.01 4.13415 14.63 3.25415 16.44 3.25415C19.51 3.25415 22 5.75415 22 8.84415C22 15.8442 15.52 19.9741 12.62 20.9641Z"
+                    stroke="#292D32"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+
+              <div className="product-image xl:mt-10">
+                <img
+                  src={product.img}
+                  className="mx-auto xl:mx-0 xl:mb-8"
+                  alt=""
+                />
+              </div>
+
+              <div className="product-desc">
+                <h3 className="font-medium uppercase">{product.name}</h3>
+
+                <div className="flex justify-between gap-1 mx-auto xl:mx-0 w-max my-2">
+                  <span className="px-2 p-[1px] border border-solid border-[#00000033]">
+                    S
+                  </span>
+
+                  <span className="px-2 p-[1px] border border-solid border-[#00000033]">
+                    M
+                  </span>
+
+                  <span className="px-2 p-[1px] border border-solid border-[#00000033]">
+                    L
+                  </span>
+
+                  <span className="px-2 p-[1px] border border-solid border-[#00000033]">
+                    XL
+                  </span>
+                </div>
+
+                <p className="font-bold text-2xl">${product.price}</p>
+              </div>
+
+              <div className="add-to-cart-icon bottom-5">
+                <a href={"#"}>
+                  <svg
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2 2H3.74001C4.82001 2 5.67 2.93 5.58 4L4.75 13.96C4.61 15.59 5.89999 16.99 7.53999 16.99H18.19C19.63 16.99 20.89 15.81 21 14.38L21.54 6.88C21.66 5.22 20.4 3.87 18.73 3.87H5.82001"
+                      stroke="#292D32"
+                      strokeWidth="1.5"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M16.25 22C16.9404 22 17.5 21.4404 17.5 20.75C17.5 20.0596 16.9404 19.5 16.25 19.5C15.5596 19.5 15 20.0596 15 20.75C15 21.4404 15.5596 22 16.25 22Z"
+                      stroke="#292D32"
+                      strokeWidth="1.5"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M8.25 22C8.94036 22 9.5 21.4404 9.5 20.75C9.5 20.0596 8.94036 19.5 8.25 19.5C7.55964 19.5 7 20.0596 7 20.75C7 21.4404 7.55964 22 8.25 22Z"
+                      stroke="#292D32"
+                      strokeWidth="1.5"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M9 8H21"
+                      stroke="#292D32"
+                      strokeWidth="1.5"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
